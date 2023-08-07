bee4643666d72a2a108e1c908bfc1c331ae565fe
andyruiruizi <141010127+andyruiruizi@users.noreply.github.com>
2023-08-07 18:46:40 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from flask import Flask, render_template, request
+import openai
+import json,time,requests
+
+
+app = Flask(__name__)
+
+@app.route("/",methods=["GET","POST"])
+def index():
+    if request.method == "POST":
+        q = request.form.get("question")
+        body = json.dumps(
+            {
+                "version": "db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf",
+                "input":
+            {
+            "promote": q
+        }
+    }
+)
+        header = {
+           'authorization': 'Token r8_W0sj98wJLyRJAiAUF4hC60DbCVRfZ7I0aTWVg',
+           'Content-Type': 'application/json'
+          }
+        output = requests.post('https://api.replicate.com/v1/predictions',data=body,headers=header)
+        time.sleep(10)
+        get_url = output.json()['urls']['get']
+        get_result = requests.post(get_url, headers=header).json()["output"]
+        
+        return(render_template("index.html",result=r["choices"][0]["message"]["content"]))
+    else:
+        return(render_template("index.html",result="waiting for your question..."))
+    
+if __name__ == "__main__":
+    app.run(port=8080)
+
+
+# In[ ]:
+
+
+
+
